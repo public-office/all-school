c089e358179b8d7db89161c42ca21fb011f1c335
Matt Lenz <matt@mattlenz.com>
2022-01-25 14:29:49 +1100

    Clear unused modules

--- components/Markdown.jsx
+++ components/Markdown.jsx
@@ -1,12 +1,7 @@
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import remarkBreaks from 'remark-breaks'
-// import remarkDirective from 'remark-directive'
 import YouTubeEmbed from 'react-lite-youtube-embed'
-// import remarkDirective from 'react-markdown-remark-directive'
-import { createElement } from 'react'
-import directive from 'remark-directive'
-import { visit } from 'unist-util-visit'
 
 export default function Markdown({ remarkPlugins = [], ...props }) {
   const plugins = [...remarkPlugins, remarkGfm, remarkBreaks]
