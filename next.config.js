e8228d76c18c0b06d3fbe4a4c699b2f64132c80f
Matt Lenz <matt@mattlenz.com>
2022-09-30 13:36:01 +1000

    Add uploads rewrite

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
+const assert = require('assert')
+
+assert(process.env.NEXT_PUBLIC_STRAPI_TOKEN, 'NEXT_PUBLIC_STRAPI_TOKEN env var not found')
+assert(process.env.NEXT_PUBLIC_STRAPI_URL, 'NEXT_PUBLIC_STRAPI_URL env var not found')
+
 module.exports = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/uploads/:path*',
+        destination: process.env.NEXT_PUBLIC_STRAPI_URL+'/uploads/:path*',
+      },
+    ]
+  },
 }
